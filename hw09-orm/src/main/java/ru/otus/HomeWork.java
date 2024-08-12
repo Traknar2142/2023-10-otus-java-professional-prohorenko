7e31247a7a8fa0255fa02f752ae25d592a8cb6da
Viktor Prokhorenko <traknar2142@gmail.com>
2024-08-12 09:25:34 +0300

    дз №9

--- hw09-orm/src/main/java/ru/otus/HomeWork.java
+++ hw09-orm/src/main/java/ru/otus/HomeWork.java
@@ -1,6 +1,7 @@
 package ru.otus;
 
 import javax.sql.DataSource;
+
 import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,9 +14,11 @@ import ru.otus.crm.service.DbServiceClientImpl;
 import ru.otus.crm.service.DbServiceManagerImpl;
 import ru.otus.jdbc.mapper.DataTemplateJdbc;
 import ru.otus.jdbc.mapper.EntityClassMetaData;
+import ru.otus.jdbc.mapper.EntityClassMetaDataImpl;
 import ru.otus.jdbc.mapper.EntitySQLMetaData;
+import ru.otus.jdbc.mapper.EntitySQLMetaDataImpl;
 
-@SuppressWarnings({"java:S125", "java:S1481"})
+@SuppressWarnings({"java:S125", "java:S1481", "java:S3740"})
 public class HomeWork {
     private static final String URL = "jdbc:postgresql://localhost:5430/demoDB";
     private static final String USER = "usr";
@@ -31,10 +34,10 @@ public class HomeWork {
         var dbExecutor = new DbExecutorImpl();
 
         // Работа с клиентом
-        EntityClassMetaData<Client> entityClassMetaDataClient; // = new EntityClassMetaDataImpl();
-        EntitySQLMetaData entitySQLMetaDataClient = null; // = new EntitySQLMetaDataImpl(entityClassMetaDataClient);
+        EntityClassMetaData<Client> entityClassMetaDataClient = new EntityClassMetaDataImpl(Client.class);
+        EntitySQLMetaData entitySQLMetaDataClient = new EntitySQLMetaDataImpl(entityClassMetaDataClient);
         var dataTemplateClient = new DataTemplateJdbc<Client>(
-                dbExecutor, entitySQLMetaDataClient); // реализация DataTemplate, универсальная
+                dbExecutor, entitySQLMetaDataClient, entityClassMetaDataClient); // реализация DataTemplate, универсальная
 
         // Код дальше должен остаться
         var dbServiceClient = new DbServiceClientImpl(transactionRunner, dataTemplateClient);
@@ -48,9 +51,9 @@ public class HomeWork {
 
         // Сделайте тоже самое с классом Manager (для него надо сделать свою таблицу)
 
-        EntityClassMetaData<Manager> entityClassMetaDataManager; // = new EntityClassMetaDataImpl();
-        EntitySQLMetaData entitySQLMetaDataManager = null; // = new EntitySQLMetaDataImpl(entityClassMetaDataManager);
-        var dataTemplateManager = new DataTemplateJdbc<Manager>(dbExecutor, entitySQLMetaDataManager);
+        EntityClassMetaData<Manager> entityClassMetaDataManager = new EntityClassMetaDataImpl(Manager.class);
+        EntitySQLMetaData entitySQLMetaDataManager = new EntitySQLMetaDataImpl(entityClassMetaDataManager);
+        var dataTemplateManager = new DataTemplateJdbc<Manager>(dbExecutor, entitySQLMetaDataManager, entityClassMetaDataManager);
 
         var dbServiceManager = new DbServiceManagerImpl(transactionRunner, dataTemplateManager);
         dbServiceManager.saveManager(new Manager("ManagerFirst"));
--- hw09-orm/src/main/java/ru/otus/jdbc/mapper/DataTemplateJdbc.java
+++ hw09-orm/src/main/java/ru/otus/jdbc/mapper/DataTemplateJdbc.java
@@ -1,40 +1,123 @@
 package ru.otus.jdbc.mapper;
 
-import java.sql.Connection;
-import java.util.List;
-import java.util.Optional;
 import ru.otus.core.repository.DataTemplate;
+import ru.otus.core.repository.DataTemplateException;
 import ru.otus.core.repository.executor.DbExecutor;
 
-/** Сохратяет объект в базу, читает объект из базы */
-@SuppressWarnings("java:S1068")
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Сохратяет объект в базу, читает объект из базы
+ */
+@SuppressWarnings({"java:S1068", "java:S112", "java:S3011"})
 public class DataTemplateJdbc<T> implements DataTemplate<T> {
 
     private final DbExecutor dbExecutor;
     private final EntitySQLMetaData entitySQLMetaData;
+    private final EntityClassMetaData<T> entityClassMetaData;
 
-    public DataTemplateJdbc(DbExecutor dbExecutor, EntitySQLMetaData entitySQLMetaData) {
+    public DataTemplateJdbc(DbExecutor dbExecutor,
+                            EntitySQLMetaData entitySQLMetaData,
+                            EntityClassMetaData<T> entityClassMetaData) {
         this.dbExecutor = dbExecutor;
         this.entitySQLMetaData = entitySQLMetaData;
+        this.entityClassMetaData = entityClassMetaData;
     }
 
     @Override
     public Optional<T> findById(Connection connection, long id) {
-        throw new UnsupportedOperationException();
+        return dbExecutor.executeSelect(connection,
+                entitySQLMetaData.getSelectByIdSql(),
+                List.of(id),
+                resultSet -> {
+                    try {
+                        ArrayList<Object> args = new ArrayList<>();
+                        while (resultSet.next()) {
+                            List<Field> allFields = entityClassMetaData.getAllFields();
+                            for (Field field : allFields) {
+                                Object arg = resultSet
+                                        .getObject(resultSet
+                                                .findColumn(field.getName()), field.getType());
+                                args.add(arg);
+                            }
+                        }
+                        return entityClassMetaData.getConstructor().newInstance(args.toArray());
+                    } catch (SQLException | InstantiationException | IllegalAccessException |
+                             InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 
     @Override
     public List<T> findAll(Connection connection) {
-        throw new UnsupportedOperationException();
+        ArrayList<Object> args = new ArrayList<>();
+        List<Field> fields = entityClassMetaData.getAllFields();
+        List<T> objects = new ArrayList<>();
+        var constructor = entityClassMetaData.getConstructor();
+        dbExecutor.executeSelect(connection,
+                entitySQLMetaData.getSelectAllSql(),
+                Collections.emptyList(),
+                resultSet -> {
+                    try {
+                        while (resultSet.next()) {
+                            for (Field field : fields) {
+                                args.add(resultSet.getObject(resultSet.findColumn(field.getName())));
+                            }
+                            objects.add(constructor.newInstance(args));
+                        }
+                    } catch (SQLException | InvocationTargetException | InstantiationException |
+                             IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                    return Collections.emptyList();
+                });
+        return objects;
     }
 
     @Override
     public long insert(Connection connection, T client) {
-        throw new UnsupportedOperationException();
+        List<Object> params = new ArrayList<>();
+        List<Field> fieldsWithoutId = entityClassMetaData.getFieldsWithoutId();
+        for (Field f : fieldsWithoutId) {
+            try {
+                f.setAccessible(true);
+                params.add(f.get(client));
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        try {
+            return dbExecutor.executeStatement(connection, entitySQLMetaData.getInsertSql(), params);
+        } catch (Exception e) {
+            throw new DataTemplateException(e);
+        }
     }
 
     @Override
     public void update(Connection connection, T client) {
-        throw new UnsupportedOperationException();
+        List<Object> params = new ArrayList<>();
+        List<Field> allFields = entityClassMetaData.getAllFields();
+        try {
+            for (Field f : allFields) {
+                f.setAccessible(true);
+                params.add(f.get(client));
+            }
+            Field idField = entityClassMetaData.getIdField();
+            idField.setAccessible(true);
+            params.add(idField.get(client));
+            dbExecutor.executeStatement(connection, entitySQLMetaData.getUpdateSql(), params);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
+
